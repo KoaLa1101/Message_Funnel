7658f620a3852e23d1bef32d94c1868be1b4c92c
OneWayDream <onewaydream@mail.ru>
2021-06-25 17:04:34 +0300

    Technical removal

--- Discord Bot/src/main/java/ru/itis/mfdiscordbot/utils/PropertiesConstants.java
+++
@@ -1,5 +0,0 @@
-package ru.itis.mfdiscordbot.utils;
-
-public class PropertiesConstants {
-    public static final String DISCORD_BOT_TOKEN = "discord.bot.token";
-}
--- Telegram Bot/src/main/java/ru/itis/mftelegrambot/utils/PropertiesConstants.java
+++
@@ -1,6 +0,0 @@
-package ru.itis.mftelegrambot.utils;
-
-public class PropertiesConstants {
-    public static final String TELEGRAM_BOT_TOKEN = "telegram.bot.token";
-    public static final String TELEGRAM_BOT_USERNAME = "telegram.bot.username";
-}
